8913076eb4d4a28be288ee01d43e22cd7c30ebc3
kq-masoni <Aky.Masoni@kenya-airways.com>
2026-07-23 15:40:25 +0300

    certificates

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,6 +3,7 @@ import Teach2Give from '../assets/Teach2Give.jpg';
 import Bunimation from '../assets/Bunimation.png';  
 import KQ from '../assets/KQ.jpg';
 import Machini from '../assets/Machini.jpg';
+import next from '../assets/next.png';
 
 const About = () => {
   const education = [
@@ -13,6 +14,9 @@ const About = () => {
   const experience=[
     {id:1 ,name:'DevSecOps Intern', description:'Currently i am a DevSecOps intern at Kenya Airways where i am learning how to implement security practices in the software development lifecycle and ensuring that applications are secure from vulnerabilities.',image: KQ },
     {id:2 ,name:'Frontend Developer Intern', description:'I was a frontend developer at Machini technologies where i gained hands-on experience in developing and maintaining web applications.',image: Machini },
+  ]
+  const certifications = [
+    { id: 1, name: 'Next.js App Router Fundamentals', description: 'Experienced with Next.js using the App Router and Pages Router to build responsive web applications. Skilled in dynamic routing, server-side rendering (SSR), static site generation (SSG), API routes, React hooks, Tailwind CSS, data fetching, state management, authentication, and deployment on Vercel.', image:next },
   ]
   return (
 
@@ -30,15 +34,16 @@ const About = () => {
               <h1>Experience</h1>
                 <div className='grid md:grid-cols-3 my-8 gap-5 cursor-pointer'>
         {
-          experience.map(job => <div key={job.id} className='service-card flex flex-col justify-center items-center p-6 text-center text-white hover-3d'>
+          experience.map(job => <div key={job.id} className='service-card flex flex-col justify-center items-center p-6 text-center gap-6 text-white hover-3d'>
             <img src={job.image} alt="" className='mb-3' width={120} height={120} />
             <h6 className='text-2xl'>{job.name}</h6><br></br>
             <p className='text-base font-light '>{job.description}</p>
           </div>)
         }
       </div>
-                <h1>Education  </h1>
             </div>
+        <div>
+                <h1 className='text-center text-3xl font-extrabold  text-cyan-400 p-10'>Education  </h1>
             {/* Education cards */}
             <div className='grid md:grid-cols-3 my-8 gap-5 cursor-pointer'>
         {
@@ -50,6 +55,21 @@ const About = () => {
         }
       </div>
         </div>
+        <div>
+                <h1 className='text-center text-3xl font-extrabold  text-cyan-400 p-10'>Certifications</h1>
+                <div className='grid md:grid-cols-3 my-8 gap-5 cursor-pointer'>
+                    {
+                        certifications.map(cert => (
+                            <div key={cert.id} className='service-card flex flex-col justify-center items-center p-6 gap-6 text-center text-white hover-3d'>
+                                <img src={cert.image} alt="" className='mb-3' width={120} height={120} />
+                                <h6 className='text-2xl'>{cert.name}</h6>
+                                <p className='text-base'>{cert.description}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+          </div>
       </div>
   
   )
